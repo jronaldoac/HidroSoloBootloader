c67d681b2fca119d2f687ffceaf7783a39edaaa8
Ronaldo <jronaldoac@hotmail.com>
2025-07-17 09:50:19 -0300

    inicialização do projeto de bootloader

---
+++ bootloader.c
@@ -0,0 +1,14 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main() {
+    stdio_init_all();
+    printf("Bootloader iniciado...\n");
+    
+    // Lógica básica para saltar para firmware atual ou factory
+    // Pseudocódigo de checagem e salto
+    
+    while (1) {
+        tight_loop_contents();
+    }
+}
